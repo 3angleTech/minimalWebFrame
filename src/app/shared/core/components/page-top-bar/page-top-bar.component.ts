d5ffac039557b93e7afc261041fbef096876815e
Catalin Andrei Dobre <catalin.dobre@3angle.tech>
2024-09-05 02:03:14 +0300

    # Adjust typography and reuse in page-top-bar

--- src/app/shared/core/components/page-top-bar/page-top-bar.component.ts
+++ src/app/shared/core/components/page-top-bar/page-top-bar.component.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-page-top-bar',
   templateUrl: './page-top-bar.component.html',
-  styleUrl: './page-top-bar.component.css',
+  styleUrl: './page-top-bar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PageTopBarComponent { }
