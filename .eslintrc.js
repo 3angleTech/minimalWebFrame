aae4796f79311d9c1e0259068b481e26cc681cb5
Catalin Andrei Dobre <catalin.dobre@3angle.tech>
2024-09-07 01:32:16 +0300

    # Adjust eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,11 @@ module.exports = {
         ]
       }
     ],
-    "no-any": 0,
+    // ESlint is too aggresive. Even if I don't use deprecated APIs, it still shows errors if the definition of the library has marked
+    // some of it's APIs as deprecated
+    "import/no-deprecated": "off",
+    "no-any": 0, // Disable ESLint no-any rule
+    "@typescript-eslint/no-explicit-any": 0,   // Disables the TypeScript-specific rule
     "capitalized-comments": "off",
     "max-params": "off",
   },
