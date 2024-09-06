568fdbbfe1c4bb4758f777ff5662e52faadea697
Catalin Andrei Dobre <catalin.dobre@3angle.tech>
2024-09-06 19:28:54 +0300

    # Add details on Profile page

--- src/app/features/profile/pages/profile/profile-page.component.ts
+++ src/app/features/profile/pages/profile/profile-page.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { IContextService, User } from '~shared/core';
+import { BasePageComponent } from '~shared/core/components/base-page/base-page.component';
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.scss'],
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent extends BasePageComponent implements OnInit {
   public currentUser!: User | undefined;
 
   constructor(
     private readonly contextService: IContextService,
-  ) {}
+  ) {
+    super();
+  }
 
   public ngOnInit(): void {
     this.currentUser = this.contextService.currentUser.value;
